03b107f4cb4509b50f82daae048ac5b0d847362b
Premchand Mahto <mpremchand23@gmail.com>
2025-05-10 20:44:52 +0530

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
-import ProjectsPage from './pages/ProjectsPage';
+import ServicesPage from './pages/ServicesPage';
+import ProfilePage from './pages/ProfilePage';
 import AboutPage from './pages/AboutPage';
+import ProjectsPage from './pages/ProjectsPage';
 import EventsPage from './pages/EventsPage';
 import TeamPage from './pages/TeamPage';
 import GalleryPage from './pages/GalleryPage';
 import TestimonialsPage from './pages/TestimonialsPage';
-import ServicesPage from './pages/ServicesPage';
 import InternshipsPage from './pages/InternshipsPage';
-import JobsPage from './pages/JobsPage';
+import JobsPage from './pages/JobsPage'; 
 import AmbassadorPage from './pages/AmbassadorPage';
-import ProfilePage from './pages/ProfilePage';
 import Footer from './components/Footer';
 import Terminal from './components/Terminal';
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+}
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="min-h-screen bg-neo-black text-white">
         <Navbar />
         <Routes>
@@ -34,6 +45,7 @@ function App() {
           <Route path="/jobs" element={<JobsPage />} />
           <Route path="/ambassador" element={<AmbassadorPage />} />
           <Route path="/profile" element={<ProfilePage />} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
         <Footer />
         <Terminal />
@@ -42,4 +54,4 @@ function App() {
   );
 }
 
-export default App;
+export default App
